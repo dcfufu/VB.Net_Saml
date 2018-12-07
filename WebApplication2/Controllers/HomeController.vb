7d5a8cbbe83933bc65fc3cfbe67e725151058653
權 S <r74616@hotmail.com>
2018-12-07 18:07:56 +0800

    get taipei open data

--- WebApplication2/Controllers/HomeController.vb
+++ WebApplication2/Controllers/HomeController.vb
@@ -2,6 +2,18 @@
     Inherits System.Web.Mvc.Controller
 
     Function Index() As ActionResult
+
+        '捷運列車到站資料
+        Dim Url As String = "https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b"
+
+        Dim JsonStr As String = ""
+        Dim objWebClient As New System.Net.WebClient
+        JsonStr = Encoding.UTF8.GetString(objWebClient.DownloadData(New Uri(Url.Trim())))
+
+        Dim Obj As Newtonsoft.Json.Linq.JObject = Newtonsoft.Json.JsonConvert.DeserializeObject(JsonStr)
+
+        Dim result As Object = Obj.Item("result")("results").ToString
+        ViewData("result") = result
         Return View()
     End Function
 
--- WebApplication2/Views/Home/Index.vbhtml
+++ WebApplication2/Views/Home/Index.vbhtml
@@ -4,6 +4,7 @@ End Code
 
 <div class="jumbotron">
     <h1>ASP.NET</h1>
+    <h2>result: @ViewData("result").</h2>
     <p class="lead">VB.NET Login keycloak.</p>
     <p><a href="/keycloakSamlServlet/About" class="btn btn-primary btn-lg">Login &raquo;</a></p>
 </div>
